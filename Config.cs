766fa559865bfb37055f5c6307ee7444b1572840
CyStuff <cypressr2581@gmail.com>
2020-07-07 16:24:06 -0600

    1.1.0 update

---
+++ Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarpChests
+{
+    public class Config
+    {
+        public string[] MasterChestRequiredItems { get; set; }
+
+        public string[] SubChestRequiredItems { get; set; }
+
+        public Config()
+        {
+            MasterChestRequiredItems = new string[] { "Star Shards" };
+            SubChestRequiredItems = new string[] { "Battery Pack" };
+        }
+    }
+}
